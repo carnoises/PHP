3792a12af60eb0b65df8bab98988cd98eda8600f
carnoises <50802944+carnoises@users.noreply.github.com>
2022-01-19 13:02:08 -0400

    Controles
    Se agregaron controles

--- Practica #1/index.php
+++ Practica #1/index.php
@@ -5,8 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/Css/estilos.css">
 </head>
 <body>
-    
+    <h1>CASA DE CAMBIOS</h1>
+
+    <table>
+        <tr>
+            <td>Monto en soles</td>
+            <td><input type="text" name="montoSoles" id=""></td>
+        </tr>
+        <tr>
+            <td>Monto en Dolares</td>
+            <td><input type="text" name="montoDolares" id=""></td>
+        </tr>
+        <tr>
+            <td>Monto en Marcos</td>
+            <td><input type="text" name="montomarcos" id=""></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Procesar"></td>
+            <td><input type="reset" value="Limpiar"></td>
+        </tr>
+    </table>
 </body>
 </html>
