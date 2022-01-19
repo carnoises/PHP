e075e2990b7a609dce52f263e7d9753c695af1ee
carnoises <50802944+carnoises@users.noreply.github.com>
2022-01-19 16:08:22 -0400

    Actulizaciones

--- Practica #1/index.php
+++ Practica #1/index.php
@@ -10,6 +10,8 @@
 <body>
     <h1>CASA DE CAMBIOS</h1>
 
+    <form action="" name="frmCambio" method="post">
+
     <table>
         <tr>
             <td>Monto en soles</td>
@@ -27,6 +29,28 @@
             <td><input type="submit" value="Procesar"></td>
             <td><input type="reset" value="Limpiar"></td>
         </tr>
+
+        <?php 
+        error_reporting(0);
+        $soles = $_POST['montoSoles'];
+        $dolares = $_POST['montoDolares'];
+        
+        ?>
+        <tr>
+            <td>Total en Soles</td>
+            <td><?php echo $soles; ?></td>
+        </tr>
+        <tr>
+            <td>Total en Dolares</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Total en Marcos</td>
+            <td></td>
+        </tr>
     </table>
+
+    </form>
+    
 </body>
 </html>
